84209fa1d230a8654102a0e853e010245ecc933e
bdickason <dickason@gmail.com>
2014-08-06 02:08:09 +0200

    Sprites are automagically now set to proper settings upon importing

--- Assets/Editor/spritePostProcessor.cs
+++ Assets/Editor/spritePostProcessor.cs
@@ -5,40 +5,15 @@ using System.Collections;
 /* spritePostProcessor - automagically post-process our sprites to save time and headaches! */
 public class spritePostProcessor : AssetPostprocessor {
 
-	// Verify deleting sprites
-	static private string deleteTitle = "Warning: Sprites were deleted";
-	static private string deleteMessage = "Do you want to delete their Prefabs as well?";
-	static private string deleteOk = "Delete";
+	private int textureSize = 32;	// Size (in pixels) of tiles we're working with (default: 32x32px)
 
-	/* OnPostprocessAllAssets - triggered any time an asset is updated/added */
-	static void OnPostprocessAllAssets (string[] importedAssets,
-		string[] deletedAssets,
-		string[] movedAssets,
-		string[] movedFromAssetPaths) {
+	/* OnPreprocessTexture - Used to force settings on sprites when imported
+	 * 	Sprites are created from a base 'texture' (.png or .jpg) */
+	void OnPreprocessTexture() {
+		TextureImporter texture = assetImporter as TextureImporter;
 
-		if(importedAssets.Length > 0) {
-			// A new asset has been imported
-			foreach(string asset in importedAssets) {
-				Debug.Log ("Imported Asset: " + asset);
-			}
-		}
-
-		if(movedAssets.Length > 0) {
-			// An asset has been renamed/moved
-			foreach(string asset in movedAssets) {
-				Debug.Log ("Moved Asset: " + asset);
-			}
-		}
-
-		if(deletedAssets.Length > 0) {
-			
-			// Prompt artist to confirm deletion of prefab
-			if(EditorUtility.DisplayDialog(deleteTitle, deleteMessage, deleteOk)) {
-				// An asset has been deleted
-				foreach(string asset in deletedAssets) {
-					Debug.Log ("Deleting prefab for: " + asset);		
-				}
-			}
-		}
+		// Apply our default settings
+		texture.filterMode =  FilterMode.Point;	// Reduces blurriness and lines between sprites
+		texture.spritePixelsToUnits = 32;		// Currently working with 32x32px textures
 	}
 }
