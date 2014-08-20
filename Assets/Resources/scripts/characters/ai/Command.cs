dde9e3644f5d08a439c0753e1d496fef5a7ca010
bdickason <dickason@gmail.com>
2014-08-19 23:01:55 -0400

    Added simple stack of commands

---
+++ Assets/Resources/scripts/characters/ai/Command.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/* Command - Base Class for a single command to be executed by an enemy */
+public class Command : MonoBehaviour {
+	
+	// Use this for initialization
+	virtual void Start();
+
+	virtual void Update();
+}
