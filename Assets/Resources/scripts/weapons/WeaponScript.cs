1af1ce62d878394f784cb7ac012430d7242848b3
radjaffo <dickasongaming@gmail.com>
2014-08-27 16:28:54 -0500

    added basic state machine to WeaponScript

--- Assets/Resources/scripts/weapons/WeaponScript.cs
+++ Assets/Resources/scripts/weapons/WeaponScript.cs
@@ -21,8 +21,19 @@ public class WeaponScript : MonoBehaviour {
 	[Tooltip("Amount to knock the character back upon impact")]
 	public float knockback = 500f;
 
+	[Tooltip("Maximum amount of time until the character can no longer link attacks")]
+	private float comboCooldown = 0f;
+
+	[Tooltip("Amount of time until character can no longer transition to second attacking state")]
+	public float firstComboCooldown = 2.0f;
+
+	[Tooltip("Amount of time until character can no longer transition to final attacking state")]
+	public float secondComboCooldown = 1.5f;
+
+
 	[Tooltip("Is this a Melee or Ranged weapon?")]
 	public string type = "Melee";
+
 
 
 	public string ownerType = "Player";	// Is a player or an enemy carrying the weapon?
@@ -36,26 +47,55 @@ public class WeaponScript : MonoBehaviour {
 	private Vector2 playerPosition;
 	private Vector2 newDirection;
 	private float playerDistance;
+	private int attackCounter;
+
 
 	private GameObject player;
 
 	// Use this for initialization
 	void Start () {
 		mainHandSlot = transform.parent.GetComponent<ItemSlotScript>();	// Grab the parent mainhand to get any slot-related info
+		attackCounter = 0;
+		player = GameObject.FindWithTag("Player");
 	}
 
 	void Update () {
 	}
 
+	void FixedUpdate () {
+				if(comboCooldown > 0) {
+					comboCooldown -= Time.deltaTime;
+					Debug.Log ("fixedupdate Combo timer is : " + comboCooldown);
+				}
+				if(comboCooldown <= 0)
+					comboCooldown = 0;
+		}
+
+
 	/* Attack - Shot triggered by another script */
 	public void Attack() {
 		if(CanAttack) {
-			// Character attacked, trigger cooldown
 			mainHandSlot.Cooldown(shootingRate);
-
+			// Character attacked, trigger cooldown
  			switch(type) {
 			case "Melee": 
 				// Handle melee weapon code here
+
+				// Only players have combos for now
+				if(ownerType == "Player")
+				{
+					//use attackCounter to decide which state to enter
+					if(attackCounter == 0)
+						LightAttack();
+
+					else if(attackCounter == 1)
+						MediumAttack();
+
+					else{
+						Debug.Log ("whatd you do to attack counter?");
+						attackCounter = 0;
+					}
+				}
 
 				// Melee attack is attached to parent (character)
 
@@ -63,8 +103,7 @@ public class WeaponScript : MonoBehaviour {
 
 			case "Ranged": 
 				// Handle ranged weapon code here
-
-				player = GameObject.FindWithTag("Player");
+				
 				// Is the player still alive?
 				if(player != null)
 				{
@@ -97,7 +136,7 @@ public class WeaponScript : MonoBehaviour {
 								shotTransform.position = transform.position;
 
 								// Figure out what direction character is facing
-								//Vector3 newDirection = new Vector3(parentMoveScript.facing.x, parentMoveScript.facing.y, 0);
+								// Vector3 newDirection = new Vector3(parentMoveScript.facing.x, parentMoveScript.facing.y, 0);
 
 								//Shoot directly at player location
 								aiPosition = new Vector2(transform.position.x , transform.position.y);
@@ -159,7 +198,7 @@ public class WeaponScript : MonoBehaviour {
 				// Calculate armor reduction
 				float totalDamage = damage - defenderHealth.armor;
 
-				// attacks should always do 1 dmg, even if they are very weak
+				// Attacks should always do 1 dmg, even if they are very weak
 				if(totalDamage <= 0) {
 					totalDamage = 1;
 				}
@@ -176,4 +215,52 @@ public class WeaponScript : MonoBehaviour {
 		var defenderMoveScript = defender.GetComponent<MoveScript>();
 		defenderMoveScript.Push(direction, amount);
 	}
+
+	/* 
+	CombinationAttack - FSM
+	Each state sets the countdown timer for transitioning
+	attackCounter is used to decide which state is called
+	*/ 
+	void LightAttack()	{
+		Debug.Log ("entering attack state 1");
+		attackCounter++;
+		// Set a time limit to link the next attack
+		comboCooldown = firstComboCooldown;
+	}
+
+	
+	// Second state - mid level attack, can transition to heavy attack or idle state
+	void MediumAttack(){
+		// Timer has not hit zero, able to transition to this state
+		if(comboCooldown > 0){
+			Debug.Log ("entering attack state 2");
+			attackCounter++;
+			// Set a time limit to link the next attack
+			comboCooldown = secondComboCooldown;
+		}
+
+		// Timer hit zero, going back to idle state
+		else{
+		Debug.Log ("didn't attack in time... combo broken");
+		attackCounter = 0;
+		}
+	
+	}
+
+	// Third state - final heavy attack, transitions back to idle state
+	void HeavyAttack(){
+
+		// Timer has not hit zero, able to transition to this state
+		if (comboCooldown > 0) {
+			Debug.Log ("entering final attack state");
+		
+		}
+
+		else{
+				Debug.Log ("didn't attack in time, final combo broken");
+		}
+		// Going back to idle state either way
+		attackCounter = 0;
+	}
+
 }
